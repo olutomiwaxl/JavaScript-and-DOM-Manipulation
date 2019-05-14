b03960fd7cbd285008bab3952cf988612e51993b
Olutomiwaxl <oloyedeolutomiwa@yahoo.com>
2019-05-13 20:29:39 -0500

    date filter/level 1 done

--- static/js/app.js
+++ static/js/app.js
@@ -54,24 +54,24 @@ var myvar = '<table>'+
 
 
 // date filter for the table 
-function dateSearch() {
+var dateSearch = function dateSearch() {
     // Declare variables 
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("datetime");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-  
-    // Loop through all table rows, and hide unneeded data
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0]; 
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      } 
-    }
-  }
+                          var input, filter, table, tr, td, i, txtValue;
+                          input = document.getElementById("datetime");
+                          filter = input.value.toUpperCase();
+                          table = document.getElementById("myTable");
+                          tr = table.getElementsByTagName("tr");
+                        
+                          // Loop through all table rows, and hide unneeded data
+                          for (i = 0; i < tr.length; i++) {
+                            td = tr[i].getElementsByTagName("td")[0]; 
+                            if (td) {
+                              txtValue = td.textContent || td.innerText;
+                              if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                                tr[i].style.display = "";
+                              } else {
+                                tr[i].style.display = "none";
+                              }
+                            } 
+                          }
+                        }
